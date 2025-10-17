ccf5a22cd5f77b4a22357e0aa894f24c606b0ef0
vaheeD khoshnouD <khoshnud@gmail.com>
2025-10-17 21:26:44 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@
   import path from 'path';
 
   export default defineConfig({
-  base: '/ravandeh-studio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
